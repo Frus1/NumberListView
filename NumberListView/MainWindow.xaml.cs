bc362ce3ab4f66e4b3307cbbcef0c087dab93792
Frus1 <165252939+Frus1@users.noreply.github.com>
2024-11-09 21:28:58 +0500

    Add files via upload

---
+++ NumberListView/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace List_of_Numbers
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int click = 0;
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NumberList.Items.Add(click);
+            click++;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            NumberList.Items.Remove(NumberList.SelectedItem);
+        }
+    }
+}
